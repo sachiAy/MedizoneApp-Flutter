bb46a45bb07f1176491fb97928d87eea2eba7253
sachiAy <sachiniayeshika943@gmail.com>
2020-02-29 23:26:28 +0530

    Add a drawer to app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Welcome to Medizone!"),
-));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('MEDIZONE'),
+            centerTitle: true,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
 
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                  ),
+                  child: Text(
+                    'Medizone',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text('Home'),
+                  // onTap: () {
+                  //   Navigator.pop(ListTile); 
+                  // },
+                ),
+                ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text('Messages'),
+                ),
+              ],
+            ),
+          ),
+
+          body: Center(
+            child: Text('Welcome to Medizone App!'),
+          ),
+
+          floatingActionButton: Center(
+            child: FloatingActionButton(
+              child: Text('Login'),
+            ),
+          )
+
+          ),
+    ));
